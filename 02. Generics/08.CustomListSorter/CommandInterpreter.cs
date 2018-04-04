d0c583149688ce3f10544396b905ac63e4c67aa7
mmihaylov82 <mmihaylov82@gmail.com>
2018-04-05 00:14:31 +1000

    Add remaining exercises

---
+++ 02. Generics/08.CustomListSorter/CommandInterpreter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+public class CommandInterpreter
+{
+    private CustomList<string> list;
+
+    public CommandInterpreter()
+    {
+        this.list = new CustomList<string>();
+    }
+
+    public void ProcessInput()
+    {
+        string inputLine;
+
+        while ((inputLine = Console.ReadLine()) != "END")
+        {
+            var inputArgs = inputLine.Split().ToList();
+
+            var command = inputArgs[0];
+            inputArgs.Remove(inputArgs.First());
+
+            switch (command)
+            {
+                case "Add":
+                    this.list.Add(inputArgs[0]);
+                    break;
+
+                case "Remove":
+                    this.list.Remove(int.Parse(inputArgs[0]));
+                    break;
+
+                case "Contains":
+                    Console.WriteLine(this.list.Contains(inputArgs[0]));
+                    break;
+
+                case "Swap":
+                    this.list.Swap(int.Parse(inputArgs[0]), int.Parse(inputArgs[1]));
+                    break;
+
+                case "Greater":
+                    Console.WriteLine(this.list.CountGreaterThan(inputArgs[0]));
+                    break;
+
+                case "Max":
+                    Console.WriteLine(this.list.Max());
+                    break;
+
+                case "Min":
+                    Console.WriteLine(this.list.Min());
+                    break;
+
+                case "Print":
+                    foreach (var item in this.list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+
+                case "Sort":
+                    this.list.Sort();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ 02. Generics/10.Tuple/CustomTuple.cs
@@ -0,0 +1,18 @@
+﻿
+public class CustomTuple<T, U>
+{
+    public CustomTuple(T itemOne, U itemTwo)
+    {
+        this.ItemOne = itemOne;
+        this.ItemTwo = itemTwo;
+    }
+
+    public T ItemOne { get; private set; }
+
+    public U ItemTwo { get; private set; }
+
+    public override string ToString()
+    {
+        return $"{this.ItemOne} -> {this.ItemTwo}";
+    }
+}
