0ed77b5a94036637822bc8446ba8eff8f8ad487b
mmihaylov82 <mmihaylov82@gmail.com>
2018-04-09 13:54:39 +0900

    Add Reflection problems 1 & 2

---
+++ 04. Reflection/01.HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Linq;
+using System.Reflection;
+
+public class HarvestingFieldsTest
+{
+    public static void Main()
+    {
+        Type type = Type.GetType("HarvestingFields");
+        FieldInfo[] allFields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+        string command;
+
+        while ((command = Console.ReadLine()) != "HARVEST")
+        {
+            ExecuteCommand(command, allFields);
+        }
+    }
+
+    private static void ExecuteCommand(string command, FieldInfo[] allFields)
+    {
+        FieldInfo[] filteredFields;
+        switch (command)
+        {
+            case "public":
+                filteredFields = allFields.Where(f => f.IsPublic).ToArray();
+                break;
+
+            case "private":
+                filteredFields = allFields.Where(f => f.IsPrivate).ToArray();
+                break;
+
+            case "protected":
+                filteredFields = allFields.Where(f => f.IsFamily).ToArray();
+                break;
+
+            case "all":
+                filteredFields = allFields;
+                break;
+
+            default:
+                throw new InvalidOperationException("Unknown command!");
+        }
+
+        PrintFilteredFields(filteredFields);
+    }
+
+    private static void PrintFilteredFields(FieldInfo[] filteredFields)
+    {
+        foreach (var field in filteredFields)
+        {
+            if (field.Attributes.ToString() == "Family")
+            {
+                Console.WriteLine($"protected {field.FieldType.Name} {field.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{field.Attributes.ToString().ToLower()} {field.FieldType.Name} {field.Name}");
+            }
+        }
+    }
+}
+
---
+++ 04. Reflection/02.BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,34 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            Type type = Type.GetType("P02_BlackBoxInteger.BlackBoxInteger");
+
+            FieldInfo innerValue = type.GetField("innerValue", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            object instance = Activator.CreateInstance(type, true);
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var commandArgs = input.Split('_');
+                var methodName = commandArgs[0];
+                var value = int.Parse(commandArgs[1]);
+
+                MethodInfo method = methods.First(m => m.Name == methodName);
+
+                method.Invoke(instance, new object[] { value });
+
+                Console.WriteLine(innerValue.GetValue(instance));
+            }
+        }
+    }
+}
